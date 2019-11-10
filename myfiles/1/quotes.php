d8aea2ce83877da1c8a66313ad2a2613e0bc67f2
Poul Larsen <plarsen85@gmail.com>
2019-11-10 13:12:08 +0100

    page 30-31.. more exercise working through the book :)

---
+++ myfiles/1/quotes.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+<head>
+
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Bootstrap CSS -->
+	<!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> -->
+	<link rel="stylesheet" type="text/css" href="css/mystyle.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap-4.3.1-dist/css/bootstrap.min.css">
+
+	<title>Hello, world!</title>
+
+</head>
+<body>
+<div class="container">
+<?php # Script 1.10 quotes.php
+
+// Set variables
+$quantity = 30; // Buying 30 widgets.
+$price = 119.95;
+$taxrate = .5; // 5% sales tax
+
+// Calculate the total:
+$total = $quantity *  $price;
+// Procent regning. K = k + (k * r)
+// $total = $total + ($total * $taxrate);
+$total += ($total * $taxrate);
+
+// format the total:
+$total = number_format($total, 2);
+
+// Print the result using double quotation marks:
+echo "<h3>Using double quotation Marks:</h3>";
+echo "<p>You are purchasing <strong>$quantity</strong> widget(s) at a cost of <strong>\$$price</strong> each.<br>With tax, the total comes to <strong>$$total</strong>.</p>\n";
+
+// Print the result using single quotation marks:
+echo '<h3>Using double quotation Marks:</h3>';
+echo '<p>You are purchasing <strong>$quantity</strong> widget(s) at a cost of <strong>\$$price</strong> each.<br>With tax, the total comes to <strong>$$total</strong>.</p>\n';
+
+?>
+</div>
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script>window.jQuery || document.write('<script src="bootstrap-4.3.1-dist/js/jquery-3.3.1.slim.min.js"><\/script>')</script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script>window.jQuery || document.write('<script src="bootstrap-4.3.1-dist/js/popper.min.js"><\/script>')</script>
+
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script>window.jQuery || document.write('<script src="bootstrap-4.3.1-dist/js/bootstrap.min.js"><\/script>')</script>
+</body>
+</html>
