e34807d6987205029c014f8eba8ea5903d681fb6
Poul Larsen <plarsen85@gmail.com>
2019-11-11 16:25:03 +0100

    reach chapter 2 in the book. Validating forms and some styling with bootstrap

--- myfiles/2/handle_form.php
+++ myfiles/2/handle_form.php
@@ -15,24 +15,61 @@
 <div class="container">
 
 <!-- Script 2.1 - form.html -->
-<?php # Script 2.5 - handle_form.php #4
+<?php # Script 2.4 - handle_form.php #3
 
-$name =$_REQUEST['name'];
-$email =$_REQUEST['email'];
-$comments =$_REQUEST['comments'];
-// Not used:
-/*
-$_REQUEST['age'];
-$_REQUEST['gender'];
-$_REQUEST['submit'];
-*/
+// Validate the name:
+if (!empty($_REQUEST['name'])) {
+	$name = $_REQUEST['name'];
+} else {
+	$name = NULL;
+	echo '<p class="text-danger">You forgot to enter your name!</p>';
+}
+// Validate the email:
+if (!empty($_REQUEST['email'])) {
+	$email = $_REQUEST['email'];
+} else {
+	$email = NULL;
+	echo '<p class="text-danger">You forgot to enter your email address!</p>';
+}
+// Validate the comments:
+if (!empty($_REQUEST['comments'])) {
+	$comments = $_REQUEST['comments'];
+} else {
+	$comments = NULL;
+	echo '<p class="text-danger">You forgot to enter your comments!</p>';
+}
 
-// Print the submitted information:
-echo "<p>Thank you, <strong>$name</strong>, for the following comments:</p>
-<pre>$comments</pre>
-<p>We will reply to you at <em>$email</em>.</p>\n";
+// Validate the gender;
+if (isset($_REQUEST['gender'])) {
+
+	$gender = $_REQUEST['gender'];
+
+	if ($gender == 'M') {
+		$greeting = '<p><strong>Good day, Sir!</strong></p>';
+	} elseif ($gender == 'F') {
+		$greeting = '<p><strong>Good day, Madam!</strong></p>';
+	} else { // Unacceptable value:
+		$gender = NULL;
+		echo '<p class="text-danger">Gender should be either "M" or "F"!</p>';
+	}
+} else { // $_REQUEST['gender'] is not set.
+	$gender = NULL;
+	echo '<p class="text-danger">You forgot to select your gender!</p>';
+}
+
+// If everything ik OK, print the message:
+if ($name && $email && $gender && $comments) {
+	echo "<p>Thank you, <strong>$name</strong>, for the following message:</p>
+	<pre>$comments</pre> 
+	<p>We will replay to you at <em>$email</em>.</p>";
+
+	echo $greeting;
+
+} else {
+	echo '<p class="text-danger">Please go back and fill out the form again.</p>';
+}
+// ========== END OF PHP SCRIPT ==========
 ?>
-
 </div>
 
 <!-- Optional JavaScript -->
