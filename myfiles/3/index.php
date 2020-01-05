191eed860a15b2490085f2a0660ad8b6a80b0a5f
Poul Larsen <plarsen85@gmail.com>
2020-01-05 12:51:28 +0100

    grinding through the book

--- myfiles/3/index.php
+++ myfiles/3/index.php
@@ -1,6 +1,19 @@
-<?php # Script 3.4 - index.php
+<?php # Script 3.7 - index.php #2
+
+// This function outputs theoretical HTML
+// for adding ads to a web page.
+function create_ad() {
+	echo '<div class="alert alert-info" role="alert">
+			<p>This is an annoying ad! This is an annoying ad! This is an annoying ad! This is an annoying ad!</p>
+			</div>';
+} // End of function
+
+
 $page_title = 'Welcome to this site!';
 include('includes/header.html');
+
+// Call the function
+create_ad();
 ?>
 
  		<!-- Begin page content -->
@@ -22,5 +35,9 @@ include('includes/header.html');
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 		<!-- End page content -->
 
+<?php 
+// Call the function again:
+create_ad();
+?>
 
 <?php include('includes/footer.html');?>
